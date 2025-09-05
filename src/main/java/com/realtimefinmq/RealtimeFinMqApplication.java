cd52326b8c6ee1606de1773d0b2704945ccf991e
ehgkals <ehgkals12@gmail.com>
2025-09-05 17:37:01 +0900

    프로젝트 생성

---
+++ src/main/java/com/realtimefinmq/RealtimeFinMqApplication.java
@@ -0,0 +1,13 @@
+package com.realtimefinmq;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RealtimeFinMqApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(RealtimeFinMqApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/realtimefinmq/RealtimeFinMqApplicationTests.java
@@ -0,0 +1,13 @@
+package com.realtimefinmq;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class RealtimeFinMqApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
